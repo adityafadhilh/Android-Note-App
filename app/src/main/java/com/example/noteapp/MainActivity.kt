cf94bfa6ac19bf731515443e3bbd09a957730279
Aditya Fadhil Herindro <af.herindro@gmail.com>
2023-08-21 12:33:37 +0700

    added android files

---
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.noteapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var rvNote: RecyclerView
+    private lateinit var fab: FloatingActionButton
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvNote = findViewById(R.id.rv_note)
+        fab = findViewById(R.id.fab_add)
+
+        rvNote.layoutManager = LinearLayoutManager(this)
+        rvNote.adapter = NoteAdapter(noteArray)
+    }
+
+    private var noteArray = arrayListOf<Note>(
+        Note(1, "Test", "suahsuahshanusahsanjsnausna"),
+        Note(2, "Test", "sasasasasadsadsasasasasasasasas"),
+        Note(3, "Test", "dasasadfsdasdadadadadadadadadadad"),
+        Note(4, "Test", "dasasadfsdasdadadadadadadadadadad"),
+        Note(5, "Test", "dasasadfsdasdadadadadadadadadadad"),
+        Note(6, "Test", "dasasadfsdasdadadadadadadadadadad"),
+    )
+}
---
+++ app/src/main/java/com/example/noteapp/NoteAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.noteapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
+import org.w3c.dom.Text
+import java.util.zip.Inflater
+
+class NoteAdapter(private val noteArray: ArrayList<Note>) : RecyclerView.Adapter<NoteAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = noteArray.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val cvTitle = holder.itemView.findViewById<TextView>(R.id.cv_title)
+        val cvContent = holder.itemView.findViewById<TextView>(R.id.cv_content)
+
+        cvTitle.text = noteArray[position].title
+        cvContent.text = noteArray[position].content
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}
